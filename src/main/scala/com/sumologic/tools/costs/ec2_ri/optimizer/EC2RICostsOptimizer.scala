bc5eb54586d8f78928b06f33d8148439a00ffac3
karooolek <karooolek@gmail.com>
2019-06-11 11:34:23 +0200

    connected to ec2 aws client, downloaded instances and RIs

--- src/main/scala/com/sumologic/tools/costs/ec2_ri/optimizer/EC2RICostsOptimizer.scala
+++ src/main/scala/com/sumologic/tools/costs/ec2_ri/optimizer/EC2RICostsOptimizer.scala
@@ -1,23 +1,35 @@
 package com.sumologic.tools.costs.ec2_ri.optimizer
 
+import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
 import com.amazonaws.regions.Regions
-import com.amazonaws.services.appsync.AWSAppSyncClientBuilder
-import com.amazonaws.services.appsync.model.ListApiKeysRequest
+import com.amazonaws.services.ec2.AmazonEC2Client
+
+import scala.collection.JavaConverters._
 
 object EC2RICostsOptimizer {
 
   def main(args: Array[String]) {
-    val apiId = args(0)
+    val awsKey = args(0)
+    val awsSecret = args(1)
 
-    val awsClient = AWSAppSyncClientBuilder.standard().
+    val ec2client = AmazonEC2Client.builder().
       withRegion(Regions.EU_CENTRAL_1).
+      withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsKey, awsSecret))).
       build()
 
-    val apiKeys = awsClient.listApiKeys(new ListApiKeysRequest().withApiId(apiId)).getApiKeys
-    apiKeys.forEach(apiKey => {
-      println(apiKey.toString)
-    })
+    val ec2instances = ec2client.describeInstances().getReservations.asScala
 
-    awsClient.shutdown()
+    println("instances:")
+    for (ec2instance <- ec2instances) {
+      println(ec2instance)
+    }
+
+    println("reserved instances:")
+    val ec2reservedInstances = ec2client.describeReservedInstances().getReservedInstances.asScala
+    for (ec2reservedInstance <- ec2reservedInstances) {
+      println(ec2reservedInstance)
+    }
+
+    ec2client.shutdown()
   }
 }
