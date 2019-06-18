50fe0d94ed7d8479cb73e325455ffbecaa498356
karooolek <karooolek@gmail.com>
2019-06-18 11:29:20 +0200

    some test cases

--- src/test/scala/com/sumologic/tools/costs/ec2_ri/optimizer/analizer/Ec2RiAnalizerSpec.scala
+++ src/test/scala/com/sumologic/tools/costs/ec2_ri/optimizer/analizer/Ec2RiAnalizerSpec.scala
@@ -1,23 +1,109 @@
 package com.sumologic.tools.costs.ec2_ri.optimizer.analizer
 
+import com.sumologic.tools.costs.ec2_ri.optimizer.reserved.{ReservedInstanceSummary, ReservedInstancesSummary}
+import com.sumologic.tools.costs.ec2_ri.optimizer.running.RunningInstancesSummary
 import org.scalatest._
-
-import scala.collection.mutable.Stack
 
 class Ec2RiAnalizerSpec extends FlatSpec with Matchers {
 
-  "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    stack.pop() should be (2)
-    stack.pop() should be (1)
+  "A EC2 to RI analizer" should "give empty output for empty inputs" in {
+    val ec2riAnalizer = new Ec2RiAnalizer(
+      RunningInstancesSummary(
+        Map[String, Double]()
+      ),
+      ReservedInstancesSummary(
+        Map[String, ReservedInstanceSummary]())
+
+    )
+    ec2riAnalizer.analize() shouldBe new Ec2RiAnalysis(
+      Map[String, Ec2RiSizeDiff](),
+      Map[String, Double](),
+      Map[String, Double](),
+      0.0,
+      0.0,
+      0.0,
+      Seq[Ec2RiConversion]()
+    )
   }
 
-  it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[Int]
-    a [NoSuchElementException] should be thrownBy {
-      emptyStack.pop()
-    }
+  it should "suggest no conversions without under/over reserved instances" in {
+    val ec2riAnalizer = new Ec2RiAnalizer(
+      RunningInstancesSummary(
+        Map[String, Double](
+          "m1" -> 5.0,
+          "c1" -> 10.0,
+          "t1" -> 15.0
+        )
+      ),
+      ReservedInstancesSummary(
+        Map[String, ReservedInstanceSummary](
+          "m1" -> ReservedInstanceSummary(5.0, 5.0),
+          "c1" -> ReservedInstanceSummary(10.0, 10.0),
+          "t1" -> ReservedInstanceSummary(15.0, 15.0)
+        )
+      )
+    )
+    ec2riAnalizer.analize().suggestedConversions.isEmpty shouldBe true
+  }
+
+  it should "suggest no conversions with overreserved m1 and without underreserved instances" in {
+    val ec2riAnalizer = new Ec2RiAnalizer(
+      RunningInstancesSummary(
+        Map[String, Double](
+          "m1" -> 3.0,
+          "c1" -> 10.0,
+          "t1" -> 15.0
+        )
+      ),
+      ReservedInstancesSummary(
+        Map[String, ReservedInstanceSummary](
+          "m1" -> ReservedInstanceSummary(5.0, 5.0),
+          "c1" -> ReservedInstanceSummary(10.0, 10.0),
+          "t1" -> ReservedInstanceSummary(15.0, 15.0)
+        )
+      )
+    )
+    ec2riAnalizer.analize().suggestedConversions.isEmpty shouldBe true
+  }
+
+  it should "suggest no conversions without overreserved instances and with underreserved m1" in {
+    val ec2riAnalizer = new Ec2RiAnalizer(
+      RunningInstancesSummary(
+        Map[String, Double](
+          "m1" -> 3.0,
+          "c1" -> 10.0,
+          "t1" -> 15.0
+        )
+      ),
+      ReservedInstancesSummary(
+        Map[String, ReservedInstanceSummary](
+          "m1" -> ReservedInstanceSummary(3.0, 3.0),
+          "c1" -> ReservedInstanceSummary(10.0, 10.0),
+          "t1" -> ReservedInstanceSummary(15.0, 15.0)
+        )
+      )
+    )
+    ec2riAnalizer.analize().suggestedConversions.isEmpty shouldBe true
+  }
+
+  it should "suggest m1->c1 conversion with overreserved m1 and underreserved c1" in {
+    val ec2riAnalizer = new Ec2RiAnalizer(
+      RunningInstancesSummary(
+        Map[String, Double](
+          "m1" -> 3.0,
+          "c1" -> 12.0,
+          "t1" -> 15.0
+        )
+      ),
+      ReservedInstancesSummary(
+        Map[String, ReservedInstanceSummary](
+          "m1" -> ReservedInstanceSummary(5.0, 5.0),
+          "c1" -> ReservedInstanceSummary(10.0, 10.0),
+          "t1" -> ReservedInstanceSummary(15.0, 15.0)
+        )
+      )
+    )
+    ec2riAnalizer.analize().suggestedConversions(0).input._1 shouldBe "m1"
+    ec2riAnalizer.analize().suggestedConversions(0).output._1 shouldBe "c1"
   }
 }
