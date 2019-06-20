2ca06030ed4c75b0a91401e39e1155e50e22b710
karooolek <karooolek@gmail.com>
2019-06-20 10:05:50 +0200

    added sl4j dependencies

--- src/main/scala/com/sumologic/tools/costs/ec2_ri/optimizer/ui/shellbase/Ec2RiShell.scala
+++ src/main/scala/com/sumologic/tools/costs/ec2_ri/optimizer/ui/shellbase/Ec2RiShell.scala
@@ -2,7 +2,7 @@ package com.sumologic.tools.costs.ec2_ri.optimizer.ui.shellbase
 
 import com.sumologic.shellbase.{ShellBase, ShellCommand}
 
-object Ec2RiShell extends  ShellBase("ec2ri-optimizer") {
+object Ec2RiShell extends ShellBase("ec2ri-optimizer") {
   override def commands: Seq[ShellCommand] = {
     Seq(
       new Ec2RiOptimizerCommandSet
